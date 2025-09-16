08dbf97595633233c463d9fb04bc4ad511a2f749
T_DaMER <dpancov64@gmail.com>
2025-09-16 19:02:24 +0300

    try to fix using the a

--- src/helpers/xlsx/saveObjectAsXlsx.ts
+++ src/helpers/xlsx/saveObjectAsXlsx.ts
@@ -22,17 +22,16 @@ export default function (fileName: string, data: BloodSample) {
   const { blob } = createXlsxBlob(data)
   const fullName = fileName + fileExtension
 
-  if (isTelegram()) {
-    console.warn('telegram')
-    const url = URL.createObjectURL(blob)
-    const a = document.createElement('a')
-    a.href = url
-    a.download = fullName
-    document.body.appendChild(a)
-    a.click()
-    a.remove()
-    URL.revokeObjectURL(url)
-  }
+  // if (isTelegram()) {
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = fullName
+  document.body.appendChild(a)
+  a.click()
+  a.remove()
+  URL.revokeObjectURL(url)
+  // }
 
-  saveAs(blob, fullName, { autoBom: true })
+  // saveAs(blob, fullName, { autoBom: true })
 }
